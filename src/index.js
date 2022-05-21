d4e6be1b01a611654220fae6ef90b633baf45b8d
Omar Shehata <omar.sameh.shehata@gmail.com>
2022-05-21 14:25:29 -0400

    Antialias

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 const controls = new OrbitControls(camera, renderer.domElement);
